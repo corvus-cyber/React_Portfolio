8211a31d12b8e15d479f0cf7b9f0ef0e53e1c356
Corvus <wyatta.hancock@gmail.com>
2020-10-12 09:54:07 -0600

    app functioning on localhost

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter, Route, Link } from "react-router-dom";
+import Footer from "./components/Footer/Footer"
+import Wrapper from "./components/Wrapper/index"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename='/'>
+      <div>
+        <Wrapper>
+        </Wrapper>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ reactportfolio/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Styles/style.css"
+
+function Footer(){
+    return (
+        <div className="row md-4 fixed-bottom">
+        <div className="col-md-12 text-muted">
+                <div className="footer-copyright text-center py-3">© 2020 Copyright:
+                <a className="copyrightLink" href="https://www.linkedin.com/in/wyatt-a-hancock-885705166/"> Wyatt A. Hancock</a>
+                </div>
+        </div> 
+        </div>
+    );
+}
+
+export default Footer;
