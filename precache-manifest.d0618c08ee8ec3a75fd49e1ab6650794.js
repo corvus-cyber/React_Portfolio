8a4e9df8d7f404ea93775cb5c5553db930880321
Corvus <wyatta.hancock@gmail.com>
2020-10-15 11:52:27 -0600

    Updates

---
+++ precache-manifest.d0618c08ee8ec3a75fd49e1ab6650794.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95dbd7279ea3017ee14137a51ca213b",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "30886c62b9a45308335b",
+    "url": "/React_Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dbce2def7e6415912a01",
+    "url": "/React_Portfolio/static/css/main.644393ee.chunk.css"
+  },
+  {
+    "revision": "30886c62b9a45308335b",
+    "url": "/React_Portfolio/static/js/2.645e92e3.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/React_Portfolio/static/js/2.645e92e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbce2def7e6415912a01",
+    "url": "/React_Portfolio/static/js/main.bc2f547a.chunk.js"
+  },
+  {
+    "revision": "631a773752e5aa1ef165",
+    "url": "/React_Portfolio/static/js/runtime-main.7e17cded.js"
+  },
+  {
+    "revision": "0806f9e06a82d2d6a6d4ef20ae82a1c9",
+    "url": "/React_Portfolio/static/media/Developer.0806f9e0.png"
+  },
+  {
+    "revision": "ae2ef42fdc8280bec4f2be2329c127b1",
+    "url": "/React_Portfolio/static/media/contactpic.ae2ef42f.jpg"
+  },
+  {
+    "revision": "42a6fa41dbd3b90e21a04da5dae7b94d",
+    "url": "/React_Portfolio/static/media/profile.42a6fa41.jpg"
+  },
+  {
+    "revision": "a8efb7cd43f01d6bef95ef4fce6e3f6f",
+    "url": "/React_Portfolio/static/media/refined-logo.a8efb7cd.png"
+  }
+]);
